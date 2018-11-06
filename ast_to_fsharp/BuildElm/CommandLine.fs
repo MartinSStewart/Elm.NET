a322224e63bcf8f2f77f0882ed016eb7c1a47895
Martin Stewart <MartinSStewart@gmail.com>
2018-11-06 22:13:09 +0100

    Renamed Build to BuildElm so it wouldn't be gitignored.

---
+++ ast_to_fsharp/BuildElm/CommandLine.fs
@@ -0,0 +1,18 @@
+﻿module CommandLine
+
+open System.Diagnostics
+
+let run (text: string): string =
+    let cmd = new Process()
+    do cmd.StartInfo.FileName <- "cmd.exe"
+    do cmd.StartInfo.RedirectStandardInput <- true
+    do cmd.StartInfo.RedirectStandardOutput <- true
+    do cmd.StartInfo.CreateNoWindow <- true
+    do cmd.StartInfo.UseShellExecute <- false
+    let _ = cmd.Start()
+
+    do cmd.StandardInput.WriteLine(text)
+    do cmd.StandardInput.Flush()
+    do cmd.StandardInput.Close()
+    do cmd.WaitForExit()
+    cmd.StandardOutput.ReadToEnd()
---
+++ ast_to_fsharp/BuildElm/Program.fs
@@ -0,0 +1,33 @@
+﻿open System
+open System.IO
+open CommandLine
+
+[<EntryPoint>]
+let main argv =
+
+    let path = Path.Combine("..", "..", "..", "..", "..", "elm_to_ast")
+    let elmJs = Path.Combine(path, "elm.js")
+    let destinationElmJs = Path.Combine("..", "..", "..", "..", "Elm.NET", "bin", "Debug", "netcoreapp2.1", "elm.js")
+    do File.Delete(destinationElmJs)
+    let command = 
+        "cd " + path + "\n" +
+        "elm make " + Path.Combine("src", "Main.elm") + " --output elm.js"
+    let _ = run command
+    let fileText = 
+        File.ReadAllText(elmJs) +
+        "\n" +
+        "\n" +
+        "var fs = require('fs');\n" +
+        "var input = fs.readFileSync('input.txt', 'utf8');" + 
+        "var files = input.split(\"\\n\").map(function (val, index, array) {\n" +
+        "    return fs.readFileSync(val, 'utf8');\n" +
+        "});\n" +
+        "var app = this.Elm.Main.init({flags: files});\n" +
+        "app.ports.response.subscribe( ( message ) => {\n" +
+        "    console.log(JSON.stringify(message));\n" +
+        "    process.exit();" +
+        "});\n"
+    do File.WriteAllText(elmJs, fileText)
+    do File.Move(elmJs, destinationElmJs)
+
+    0
